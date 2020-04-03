9651edf943f4529f5eef57ba05d536543e2f99f0
Vladyslav <ganushkevychv@gmail.com>
2020-04-03 20:31:27 +0200

    new logic added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 plastic.onmousedown = function(event){
     plastic.style.position = 'absolute';
     plastic.style.zIndex = 1000;
@@ -18,3 +19,60 @@ plastic.onmousedown = function(event){
         plastic.onmouseup = null;
     };
 };
+
+plastic1.onmousedown = function(event){
+    plastic1.style.position = 'absolute';
+    plastic1.style.zIndex = 1000;
+    document.body.append(plastic1);
+    moveAt(event.pageX, event.pageY);
+
+    function moveAt(pageX, pageY){
+        plastic1.style.left = pageX - plastic1.offsetWidth / 2 + 'px';
+        plastic1.style.top = pageY - plastic1.offsetHeight / 2 + 'px';
+    };
+    function onMouseMove(event){
+        moveAt(event.pageX, event.pageY);
+    };
+    document.addEventListener('mousemove', onMouseMove);
+
+    plastic1.onmouseup = function(){
+        document.removeEventListener('mousemove', onMouseMove);
+        plastic1.onmouseup = null;
+    };
+};
+match = () => {
+    let pCan = document.getElementsByClassName('pCan');
+    let plastic = document.getElementsByClassName('plastic');
+    if (plastic === pCan){
+        plastic.style.backgroundImage = 'none';
+    } else {
+
+    }
+}
+match()
+*/
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  function drop(ev) {
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+  overlap = () => {
+
+  let plastic = document.getElementById("plastic").getBoundingClientRect();
+  let plastic1 = document.getElementById("plastic1").getBoundingClientRect();
+  let plastic2 = document.getElementById("plastic2").getBoundingClientRect();
+  let plastic3 = document.getElementById("plastic3").getBoundingClientRect();
+  let plastic4 = document.getElementById("plastic4").getBoundingClientRect();
+  let plastic5 = document.getElementById("plastic5").getBoundingClientRect();
+  let pCan = document.getElementById("pCan").getBoundingClientRect();
+ if (plastic.right < pCan.left || plastic.left > pCan.right || plastic.bottom < pCan.top || plastic.top > pCan.bottom ){
+    plastic.style.display = "none";
+ }
+}
